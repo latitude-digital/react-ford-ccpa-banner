bd78db9aed10de32804655296a21336c887ca3e1
Noah Gambee <noah@latitudedigital.com>
2024-05-02 12:22:18 -0600

    2024 privacy footer updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ccpaStyles.scss';
 import L from './localization/get-localized-string.js';
-
+// import ccpaIcon from '../ccpa-icon-ford_275x132.png';
 function CAPrivacyBanner (props) {
   const {
     team = 'ford',
@@ -19,10 +19,12 @@ function CAPrivacyBanner (props) {
   const deletePILink = team === 'ford' ? 'https://www.ford.com/help/privacy/ccpa/?ccpatype=deletemypi' : 'https://www.lincoln.com/help/privacy-terms/ccpa/?ccpatype=deletemypi';
   const doNotSellPILink = team === 'ford' ? 'https://www.ford.com/help/privacy/ccpa/?ccpatype=donotsellmypi' : 'https://www.lincoln.com/help/privacy-terms/ccpa/?ccpatype=donotsellmypi';
   const privacyLink = team === 'ford' ? 'https://www.ford.com/help/privacy/' : 'https://www.lincoln.com/help/privacy-terms/#privacy';
-  const CAPrivacyLink = team === 'ford' ? 'https://www.ford.com/help/privacy/#caPrivacy' : 'https://www.lincoln.com/help/privacy-terms/#caPrivacy';
+  const CAPrivacyLink = team === 'ford' ? 'https://www.ford.com/help/privacy/ccpa/' : 'https://www.lincoln.com/help/privacy-terms/#caPrivacy';
+  const internetBasedAdsLink = team === 'ford' ? 'https://www.ford.com/help/privacy/#USprivacypolicy' : 'https://www.lincol.com/help/privacy/#USprivacypolicy';
   const privacyText = L('privacy', language);
-  const CAprivacyText = L('CAPrivacyRights', language);
+  const privacyRightsText = L('privacyRights', language);
   const cookieSettingsText = L('cookieSettings', language);
+  const internetBasedAdsText = L('internetBasedAds', language);
 
   function openCookieSettings() {
     const { Optanon } = window;
@@ -36,7 +38,7 @@ function CAPrivacyBanner (props) {
   return (
     <div className={`${team}`}>
     <div className={`ca-banner`}>
-      <div className={`container-${team}`} style={containerStyle && containerStyle}>
+      {/* <div className={`container-${team}`} style={containerStyle && containerStyle}>
         <p className={`header-${team}`}>{L('californiaPrivacyHeader', language)}</p>
         <p className={`content-${team}`}>{L('californiaPrivacyContent', language)}
           <a href={`${deletePILink}`} className={`contentLink-${team}`} target='_blank'>{L('here', language)}</a >.
@@ -46,7 +48,7 @@ function CAPrivacyBanner (props) {
             {L('californiaPrivacyButton', language)}
           </button>
         </a >
-      </div>
+      </div> */}
       {showPrivacy ?
         <div className={`privacyContainer-${team}`} style={privacyContainerStyle && privacyContainerStyle}>
           {team === 'lincoln' ? <img src={'https://d2n145t5d2n6zs.cloudfront.net/lincoln_logo_whitesymbol.png'} alt={'Lincoln Logo'} width={20} style={{marginRight: '15px', verticalAlign: 'middle', ...imgStyle}}/> : null }
@@ -54,7 +56,13 @@ function CAPrivacyBanner (props) {
           {addDivider(team === 'lincoln')}
           <span style={{ cursor: 'pointer' }} className={`privacyContainerLink-${team}`} onClick={openCookieSettings}>{cookieSettingsText}</span >
           {addDivider(team === 'lincoln')}
-          <a href={`${CAPrivacyLink}`} className={`privacyContainerLink-${team}`} target='_blank'>{CAprivacyText}</a >
+          <div style={{display: 'inline'}}>
+            <a href={`${CAPrivacyLink}`} className={`privacyContainerLink-${team}`} style={{paddingRight: '10px'}}target='_blank'>{privacyRightsText}</a >
+            <img src='https://cdn.latitudewebservices.com/icons/ccpa-icon-ford.png' style={{width: '40px', verticalAlign: 'middle', paddingRight: '15px'}}/>
+          </div>
+            
+          {addDivider(team === 'lincoln')}
+          <a href={`${internetBasedAdsLink}`} className={`privacyContainerLink-${team}`} target='_blank'>{internetBasedAdsText}</a >
           {
             otherLinks.map((link) => {
               return (
